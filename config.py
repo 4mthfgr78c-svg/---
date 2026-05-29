379d1a7d70984da9b3ca6b7c1b7124bda026ca50
4mthfgr78c-svg <4mthfgr78c@privaterelay.appleid.com>
2026-05-29 11:25:59 +1100

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = "8812711404:AAGMMOmyyF7AO2q7E4e1D6IDZHL7t92Lnf4"
 
 # Telegram ID менеджеров (можно несколько)
-MANAGER_IDS = [123456789, 987654321]  # замените на реальные
+MANAGER_IDS = [1896036065, 987654321]  # замените на реальные
 
 # Путь к БД
 DB_PATH = os.path.join(os.path.dirname(__file__), "cleaning.db")
