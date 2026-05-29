4280e3aaacf11f1239b39b8c092db343fbfbbb98
4mthfgr78c-svg <4mthfgr78c@privaterelay.appleid.com>
2026-05-29 11:48:43 +1100

    Update states.py

--- states.py
+++ states.py
@@ -5,10 +5,14 @@ class CreateObject(StatesGroup):
     waiting_for_address = State()
 
 class AssignShift(StatesGroup):
-    waiting_for_worker = State()      # выбираем из списка, не ввод ID
+    waiting_for_worker = State()
     waiting_for_object = State()
     waiting_for_start = State()
     waiting_for_end = State()
 
 class SupplyOrder(StatesGroup):
     waiting_for_text = State()
+
+class EditObject(StatesGroup):
+    waiting_for_new_name = State()
+    waiting_for_new_address = State()
